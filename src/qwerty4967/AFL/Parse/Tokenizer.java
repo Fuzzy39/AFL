60e2feaf9d9fc75f2d202e3e223137a96550ed12
Mason S Hill <2023hillms@sa.hccsc.us>
2021-09-05 21:44:43 -0400

    start parser

--- src/qwerty4967/AFL/Parse/Tokenizer.java
+++ src/qwerty4967/AFL/Parse/Tokenizer.java
@@ -1,5 +1,9 @@
 package qwerty4967.AFL.Parse;
 
+import java.util.ArrayList;
+
+import qwerty4967.AFL.Function.*;
+
 public class Tokenizer 
 {
 	// The first class that is actually particularly new to AFL!
@@ -7,5 +11,8 @@ public class Tokenizer
 	
 	// well, it basically does what used to be pass one in Ziker.
 	// it takes a string and turns them into tokens.
-
+	protected AFLFunction Tokenize (ArrayList<String> code)
+	{
+		
+	}
 }
